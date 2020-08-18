0d0b4b20836d8d95a0dbefb0ba4cc3676358161b
closekn <close.me.kn@gmail.com>
2020-08-19 06:35:02 +0900

    DB design and Model building

---
+++ src/database/migrations/2020_08_18_205202_create_posts_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('user_id');
+            $table->text('photo');
+            $table->string('caption');
+            $table->softDeletes();
+            $table->timestamps();
+
+            $table->index('id');
+            $table->index('user_id');
+
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
