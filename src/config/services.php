b8c696c34f6a34ed54db5a33cd160bff09fb1b06
closekn <close.me.kn@gmail.com>
2020-09-03 06:01:15 +0900

    fix githubOAuth redirect path

--- src/config/services.php
+++ src/config/services.php
@@ -17,7 +17,7 @@ return [
     'github' => [
         'client_id' => env('GITHUB_CLIENT_ID'),
         'client_secret' => env('GITHUB_CLIENT_SECRET'),
-        'redirect' => '/login/github/callback',
+        'redirect' => 'https://photo-posting-sns.herokuapp.com/login/github/callback',
     ],
 
     'mailgun' => [
